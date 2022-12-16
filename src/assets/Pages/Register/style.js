deacb6bf1a154757ae95cea88f8899fb7e12243b
aletanus <alessandro@altadesign.com.br>
2022-12-16 01:31:49 -0300

    Updated version.

--- src/assets/Pages/Register/style.js
+++ src/assets/Pages/Register/style.js
@@ -21,8 +21,9 @@ export const StyledRegister = styled.div`
     nav {
         display: flex;
         align-items: center;
-        justify-content: space-evenly;
-        width: 100%;
+        justify-content: space-between;
+        width: 100%;    
+        max-width: 304px;
     }
 
     nav > .gray-button{
